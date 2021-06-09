41bded069bef4c854666805f8f1ec71418bd86e8
gracyaneoliveira <gracyane_oliveira@hotmail.com>
2021-06-08 22:17:09 -0300

    Implemented test challenge TDD Event-City

---
+++ src/main/java/com/devsuperior/bds02/services/exceptions/ResourceEntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.bds02.services.exceptions;
+
+public class ResourceEntityNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceEntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
